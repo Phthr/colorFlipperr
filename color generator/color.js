9362348322331e5659f11839b176766d95263508
Phthr <105526859+Phthr@users.noreply.github.com>
2022-06-22 21:25:16 +0100

    Add files via upload

---
+++ color generator/color.js
@@ -0,0 +1,18 @@
+const colors = [ 'green','red','rgba(133,122,200)','yellow','orange','blue','pink','indigo','violet','gold','purple','cream'];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function () {
+ const randomNUmber = getRandomNumber(); //to get random number btwn 0 - 3
+ document.body.style.backgroundColor = colors[randomNUmber];
+ color.textContent = colors[randomNUmber];
+});
+
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+
+
+    
+}
+console.log('hello')
